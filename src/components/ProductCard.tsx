48f900bc60cd8e0fe797f17b7ce65894505e8c46
Aaron Carrasco <vaaroncarrasco@gmail.com>
2021-11-27 03:55:07 -0500

    Version 1.0.0-rc ready

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -23,8 +23,7 @@ export interface Props {
 // * Making it a HOC + Context provider
 export const ProductCard = ({ children, product, className, style, onChange, value, initialValues }: Props) => {
 
-  const { counter, increaseBy, maxCount, isMaxCountReached, reset }
-    = useProduct({ onChange, product, value, initialValues });
+  const { counter, increaseBy, maxCount, isMaxCountReached, reset } = useProduct({ onChange, product, value, initialValues });
 
   return (
     <Provider value={{ counter, increaseBy, product, maxCount }}>
